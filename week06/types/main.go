e3da4957f0e4937d529fd02a1aa18d259e022a59
asdf <hergduk1@naver.com>
2025-09-29 18:14:58 +0900

    .gitignore

--- week06/types/main.go
+++ week06/types/main.go
@@ -38,16 +38,24 @@ func main() {
 	// fmt.Println("학번은", id, reflect.TypeOf(id), ", 이름은", name, reflect.TypeOf(name))
 	// fmt.Println("평점 : ", gpa, reflect.TypeOf(gpa))
 
-	//Zero values
-	var f64 float64
-	var t bool
-	var s string
-	var i int
-	var i16 int16
+	// //Zero values
+	// var f64 float64
+	// var t bool
+	// var s string
+	// var i int
+	// var i16 int16
 
+	// fmt.Println(f64, reflect.TypeOf(f64))
+	// fmt.Println(t, reflect.TypeOf(t))
+	// fmt.Println(s, reflect.TypeOf(s))
+	// fmt.Println(i, reflect.TypeOf(i))
+	// fmt.Println(i16, reflect.TypeOf(i16))
+
+	var f64 float64
+	// total_price := 1000
+	// totalprice := 1000
+	totalPrice := 1000
+
+	fmt.Println(totalPrice)
 	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
-	fmt.Println(i16, reflect.TypeOf(i16))
 }
