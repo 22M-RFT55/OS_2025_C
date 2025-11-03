c0f8ca7606b9b5d6088b591f6d7b36dbf37063ee
asdf <hergduk1@naver.com>
2025-11-03 17:42:37 +0900

    week10 package v0.4

--- week10/cmd/ch04-ex04/main.go
+++ week10/cmd/ch04-ex04/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"week10/pkg/greeting"
 	"week10/pkg/greeting/deutsch"
-	"week10/pkg/greeting/korean"
+	korean "week10/pkg/greeting/korean"
 )
 
 func main() {
--- week10/pkg/greeting/korean/korean.go
+++ week10/pkg/greeting/korean/korean.go
@@ -1,11 +1,12 @@
-package dansk
+package korean
 
 import "fmt"
 
-func Hej() {
-	fmt.Println("Hej!")
+func Anyunghaseyo() {
+	fmt.Println("안녕하세요?")
 }
 
-func GodMorgen() {
-	fmt.Println("God morgen!")
+// func anyung() { // 함수명이 소문자로 시작하는 경우 외부 파일에서 접근 불가
+func Anyung() {
+	fmt.Println("안녕~")
 }
