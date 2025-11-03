cbc26b0386ad66814533691a7dc0bb37c31ee871
asdf <hergduk1@naver.com>
2025-11-03 17:12:55 +0900

    week10 package v0.2

---
+++ week10/cmd/ch04-ex02/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"week10/pkg/keyboard"
+)
+
+func main() {
+	fmt.Print("점수 입력 : ")
+	score, err := keyboard.GetFloat()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if score >= 80 {
+		fmt.Printf("%.2f점은 합격 입니다! \n", score)
+	} else {
+		fmt.Printf("%.2f점은 합격 입니다! \n", score)
+	}
+}
